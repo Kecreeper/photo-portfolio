991ef7c9cb46d5772fe8d0ef7cff1868fc354989
Kecreeper <Kecreeper@gmail.com>
2024-08-28 15:28:57 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <div className=''>
+      <div className='text-center'>
+        Photo Portfolio
+      </div>
+    </div>
+  )
+}
+
+export default App
